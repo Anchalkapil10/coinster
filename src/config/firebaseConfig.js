fa2f73a2951df7338df00c88479c67ce44930e0b
Harsh <harshk0056@gmail.com>
2023-08-28 02:34:18 +0530

    first

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCA-fWX3FGo0u8EKLwYu8X7jcuy6GnQZmg",
+  authDomain: "coinster-dc922.firebaseapp.com",
+  projectId: "coinster-dc922",
+  storageBucket: "coinster-dc922.appspot.com",
+  messagingSenderId: "155895370333",
+  appId: "1:155895370333:web:941c76d4603538adeb73b4"
+};
+
+export default firebaseConfig;
